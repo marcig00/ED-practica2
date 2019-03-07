5da6a1dd5b899e2ce9607bc2d4d3dfa1275f24e7
Manuela <Manuela@10.130.18.238>
2019-03-07 19:45:56 +0100

    nueva clase para compilar todos los tests

---
+++ edi-a002-2019/src/ule/edi/limitedpriorityqueue/AllTest.java
@@ -0,0 +1,25 @@
+package ule.edi.limitedpriorityqueue;
+
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({LimitedPriorityQueueLinkedTests .class, LimitedPriorityQueueArrayTests.class, LinkedQueueTest.class})
+
+
+public class AllTest {
+
+}
+
+
+
+
+
+
+
+
+
+
+
